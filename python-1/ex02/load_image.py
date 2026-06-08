aea2d74f18099b16c2fea3079ea8fcd1bb9a4686
Fede1109 <federicojose2000@gmail.com>
2026-06-08 22:30:34 +0200

    feat: load_image

---
+++ python-1/ex02/load_image.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import numpy as np
+
+
+def ft_load(path: str) -> np.ndarray:
+    """Loads an image, displays its format and shape, and returns its RGB pixel array.
+
+    Args:
+        path (str): The path to the image file.
+
+    Returns:
+        np.ndarray: The image data as a NumPy array, or None if an error occurs.
+    """
+    try:
+        img = Image.open(path)
+        if img.format not in ["JPEG", "MPO"]:
+            raise ValueError("Unsupported image format. Only JPG and JPEG are allowed.")
+        img_rgb = img.convert("RGB")
+        img_array = np.array(img_rgb)
+        print(f"The shape of image is: {img_array.shape}")
+
+        return img_array
+    except FileNotFoundError:
+        print(f"Error: The file at '{path}' was not found.")
+        return None
+    except Exception as error:
+        print(f"Error: {error}")
+        return None
+    
